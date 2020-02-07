5cd3f1b9e33fc48258137d0dae514450b4c04744
rox-dotcom <roxarandagr@gmail.com>
2020-02-06 18:58:13 -0600

    Pendiente

--- Prisma.cpp
+++ Prisma.cpp
@@ -8,10 +8,21 @@ public:
     Figura(int _x)
     {
         x = _x;
+        cout << "Hola soy una figura" << endl;
     }
     virtual float perimetro ();
     virtual float area();
 };
+
+class Circulo: public Figura 
+{
+    public: 
+        Circulo(int r): Figura(r)
+         {
+             cout<< "Hola soy el hijo y soy un circulo"<< endl;
+        }
+};
+
 
 main() {
 
